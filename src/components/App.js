7c649c123ab1f09ad3407d54603eccc5ee796983
mungocactus <paul.hess@blueyonder.co.uk>
2019-08-19 22:24:21 +0100

    added clock component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from './Clock';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <Clock />
+      </header>
+    )
+  }
+}
+
+export default Header
