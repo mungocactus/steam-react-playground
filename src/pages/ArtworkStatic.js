a7f7ca48fa33ebab2582624e89308863f2163ce5
mungocactus <paul.hess@blueyonder.co.uk>
2020-01-29 22:35:18 +0000

    fixed randon user code

--- src/pages/ArtworkStatic.js
+++ src/pages/ArtworkStatic.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Artwork from '../components/Artwork';
 import Basket from '../components/Basket';
+import artworks from '../components/Artworks';
 
 class ArtworkStatic extends React.Component {
   constructor() {
@@ -10,21 +11,23 @@ class ArtworkStatic extends React.Component {
     this.state = {
       artworks: {}
     };
-
-    // this.getSampleArworks = this.getSampleArworks.bind(this);
   }
 
-  getSampleArworks = () => {
-    console.log('artworks in here');
+  componentWillMount() {
+    this.setState({
+      artworks: artworks
+    })
   }
-
 
   render() {
+    console.log(this.state.artworks);
     return (
       <div className='content'>
         <Navigation />
         <h1>Artwork for Sale</h1>
-        <Artwork />
+        <ul>
+          {Object.keys(this.state.artworks).map(art => <Artwork key={art} artinfo={this.state.artworks[art]} />)}
+        </ul>
         <Basket />
       </div>
     )
--- src/pages/RandomUser.js
+++ src/pages/RandomUser.js
@@ -13,18 +13,12 @@ class RandomUser extends React.Component {
 
     console.log(this.state);
 
-    this.getRandomUser = this.getRandomUser.bind(this);
+    this.randomUser = this.randomUser.bind(this);
     this.getRandomImage = this.getRandomImage.bind(this);
+    this.getRandomUser = this.getRandomUser.bind(this);
   }
 
-  getRandomImage() {
-
-    console.log('chinaman');
-
-  }
-
-  getRandomUser(event) {
-    event.preventDefault();
+  randomUser() {
     fetch('https://randomuser.me/api/?results=1')
     .then(response => response.json())
     .then(
@@ -34,7 +28,21 @@ class RandomUser extends React.Component {
         });
       }
     )
+  }
 
+  getRandomImage() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    this.randomImage = `/random-users/random-user-${randomNumber}.jpg`;
+  }
+
+  componentWillMount() {
+    this.randomUser();
+    this.getRandomImage();
+  }
+
+  getRandomUser(event) {
+    event.preventDefault();
+    this.randomUser();
     this.getRandomImage();
   }
 
@@ -47,28 +55,27 @@ class RandomUser extends React.Component {
     return (
       <div className='content randomuser'>
         <Navigation />
-        <h1>Get your Random User</h1>
         <div className='user-content'>
-          <img src='/random-users/random-user-5.jpg' alt='random user' />
-            <h3>Name</h3>
-            {results.map(result => (
-              <div className='info-container' key='result'>
-                <p key='first'>{result.name.first}</p>
-                <p key='last'>{result.name.last}</p>
-              </div>
-            ))}
-            <h3>Address</h3>
-            {results.map(result => (
-              <div className='info-container' key='result'>
-                <p key='number'>{result.location.street.number}</p>
-                <p key='street'>{result.location.street.name},</p>
-                <p key='city'>{result.location.city},</p>
-                <p key='state'>{result.location.state},</p>
-                <p key='country'>{result.location.country}</p>
-              </div>
-            ))}
+          <img src={this.randomImage} alt='random user' />
+          <h3>Name</h3>
+          {results.map(result => (
+            <div className='info-container' key='result'>
+              <p key='first'>{result.name.first}</p>
+              <p key='last'>{result.name.last}</p>
+            </div>
+          ))}
+          <h3>Address</h3>
+          {results.map(result => (
+            <div className='info-container' key='result'>
+              <p key='number'>{result.location.street.number}</p>
+              <p key='street'>{result.location.street.name},</p>
+              <p key='city'>{result.location.city},</p>
+              <p key='state'>{result.location.state},</p>
+              <p key='country'>{result.location.country}</p>
+            </div>
+          ))}
+          <button onClick={this.getRandomUser}>Get New User</button>
         </div>
-        <button onClick={this.getRandomUser}>Get User</button>
       </div>
     )
   }
