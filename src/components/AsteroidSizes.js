c1aeb14296244dd36c24a95810dcda03ab556897
paolohess <paul.hess@bedegaming.com>
2020-06-12 10:53:48 +0100

    tidied up the styles on asteroids for mobile

--- src/components/AsteroidSizes.js
+++ src/components/AsteroidSizes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { convertToMeters, convertToThousands } from '../helpers'
+import { convertToMeters, convertToThousands, convertToMetersThousands } from '../helpers'
 
 class AsteroidSizes extends React.Component {
 
@@ -12,7 +12,7 @@ class AsteroidSizes extends React.Component {
       <li style={dangerous ? {color:'red'} : {color:'#999'}}>
         <div className='circle' style={dangerous ? {height:diameter + 'px', width:diameter + 'px', backgroundColor:'red'} : {height:diameter + 'px', width:diameter + 'px', backgroundColor:'#999'}}></div>
         <h2 className='name'>{asteroidSizes.name.slice(1, -1)}</h2>
-        <p>{convertToMeters(asteroidSizes.estimated_diameter.kilometers.estimated_diameter_max)} m</p>
+        <p>{convertToMetersThousands(asteroidSizes.estimated_diameter.kilometers.estimated_diameter_max)} m</p>
         <p>{convertToThousands(asteroidSizes.close_approach_data["0"].relative_velocity.kilometers_per_hour)} kmph</p>
         <p className='distance'>{convertToThousands(asteroidSizes.close_approach_data["0"].miss_distance.kilometers)} km</p>
       </li>
--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,14 @@ export function removeDecimal(number) {
   return Math.round(number);
 }
 
+export function convertToMeters(number) {
+  return Math.round(number * 1000);
+}
+
 export function convertToThousands(number) {
   return Math.round(number).toLocaleString();
 }
 
-export function convertToMeters(number) {
+export function convertToMetersThousands(number) {
   return Math.round(number * 1000).toLocaleString();
 }
