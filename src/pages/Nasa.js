68a116dd43dffe58c152a2971bf1b21298380b42
paolohess <paul.hess@bedegaming.com>
2020-06-13 14:54:28 +0100

    fix width on images

--- src/pages/Nasa.js
+++ src/pages/Nasa.js
@@ -55,63 +55,67 @@ class Nasa extends React.Component {
     console.log('asteroids', asteroids);
 
     return (
-      <div className='content nasa'>
-        <Header />
-        <Navigation />
-        <section>
-          <div className='nasa-image'>
-            <img src={image.url} alt='nasa of the day' />
-          </div>
-          <div className='nasa-info'>
-            <h1>{image.title}</h1>
-            <p>{image.explanation}</p>
-          </div>
-        </section>
-        <section className='asteroid-belt'>
-          <h1>Daily Asteroids are Go!</h1>
-          <p>Each day NASA publishes a list of asteroids based on when they are at their closest point to Earth.</p>
-          <p className='red'>Those in red are considered potentially hazardous to Earth.</p>
-        </section>
-        <section className='asteroid-sizes'>
-          <h1>Asteroids</h1>
-          <h2>NAME</h2>
-          <p>size</p>
-          <p>speed</p>
-          <p>closest distance to Earth</p>
-          <ul>
-            {Object.keys(asteroids).map(key =>
-              <AsteroidSizes
-              key={key}
-              index={key}
-              asteroidSizes={asteroids[key]}
-              />)}
-          </ul>
-        </section>
-        <section className='asteroid-speeds'>
-          <h1>Asteroid Speeds</h1>
-          <ul>
-            {Object.keys(asteroids).map(key =>
-              <AsteroidSpeeds
-              key={key}
-              index={key}
-              asteroidSpeeds={asteroids[key]}
-              />)}
-            <li className='asteroid-speed-earth'>
-              <h2 className='name-speed'>Earth</h2>
-              <p className='speed'>107,000 kmph</p>
-            </li>
-            <li className='asteroid-speed-iss'>
-              <h2 className='name-speed'>ISS</h2>
-              <p>International Space Station</p>
-              <p className='speed'>17,150 kmph</p>
-            </li>
-            <li className='asteroid-speed-sound'>
-              <h2 className='name-speed'>Speed of Sound</h2>
-              <p className='speed'>1,236 kmph</p>
-            </li>
-          </ul>
-        </section>
-      </div>
+      <React.Fragment>
+        <div className='content nasa'>
+          <Header />
+          <Navigation />
+          <section>
+            <div className='nasa-image'>
+              <img src={image.url} alt='nasa of the day' />
+            </div>
+            <div className='nasa-info'>
+              <h1>{image.title}</h1>
+              <p>{image.explanation}</p>
+            </div>
+          </section>
+        </div>
+        <div className='content asteroids'>
+          <section className='asteroid-belt'>
+            <h1>Daily Asteroids are Go!</h1>
+            <p>Each day NASA publishes a list of asteroids based on when they are at their closest point to Earth.</p>
+            <p className='red'>Those in red are considered potentially hazardous to Earth.</p>
+          </section>
+          <section className='asteroid-sizes'>
+            <h1>Asteroids</h1>
+            <h2>NAME</h2>
+            <p>size</p>
+            <p>speed</p>
+            <p>closest distance to Earth</p>
+            <ul>
+              {Object.keys(asteroids).map(key =>
+                <AsteroidSizes
+                key={key}
+                index={key}
+                asteroidSizes={asteroids[key]}
+                />)}
+            </ul>
+          </section>
+          <section className='asteroid-speeds'>
+            <h1>Asteroid Speeds</h1>
+            <ul>
+              {Object.keys(asteroids).map(key =>
+                <AsteroidSpeeds
+                key={key}
+                index={key}
+                asteroidSpeeds={asteroids[key]}
+                />)}
+              <li className='asteroid-speed-earth'>
+                <h2 className='name-speed'>Earth</h2>
+                <p className='speed'>107,000 kmph</p>
+              </li>
+              <li className='asteroid-speed-iss'>
+                <h2 className='name-speed'>ISS</h2>
+                <p>International Space Station</p>
+                <p className='speed'>17,150 kmph</p>
+              </li>
+              <li className='asteroid-speed-sound'>
+                <h2 className='name-speed'>Speed of Sound</h2>
+                <p className='speed'>1,236 kmph</p>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </React.Fragment>
     )
   }
 }
